7078dbc0b6a7880217656a3a3e8f98f86e1a410d
rmcampigotto <rmcampigotto@gmail.com>
2025-06-06 22:26:51 -0300

    fix: folder name

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { TasksModule } from './task/tasks.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
